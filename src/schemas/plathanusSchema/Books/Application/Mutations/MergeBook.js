d1bc1c996336cb136db3dfe2e422eedd272a23d8
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-09-29 18:48:43 +0100

    will add user logged in on book domain

--- src/schemas/plathanusSchema/Books/Application/Mutations/MergeBook.js
+++ src/schemas/plathanusSchema/Books/Application/Mutations/MergeBook.js
@@ -4,6 +4,7 @@ import deleteBook from '../../Infrastructure/deleteBook.js'
 import validator from '../_validator/index.js'
 
 export default (_, { id, deleteIt, bookData }, ctx) => {
+  if (!ctx.user.id) return ctx.core.errorHandling('You are not allowed to do that!', 'not_allowed')
   if (!deleteIt) {
     validator(ctx, bookData)
     return id ? updateBook(ctx, id, bookData) : createBook(ctx, bookData)
--- src/schemas/plathanusSchema/Books/Application/Queries/Books.js
+++ src/schemas/plathanusSchema/Books/Application/Queries/Books.js
@@ -1,3 +1,6 @@
 import readBook from '../../Infrastructure/readBook.js'
 
-export default (_, { id, deleted }, ctx) => readBook(ctx, { id, deleted })
+export default (_, { id, deleted }, ctx) => {
+  if (!ctx.user.id) return ctx.core.errorHandling('You are not allowed to do that!', 'not_allowed')
+  return readBook(ctx, { id, deleted })
+}
