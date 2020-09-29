2b72a496726f22019d43cf246163f1d69d66f3fc
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-09-29 15:07:37 +0100

    will update typedefs and resolvers with users domain

--- src/schemas/plathanusSchema/resolvers.js
+++ src/schemas/plathanusSchema/resolvers.js
@@ -1,12 +1,16 @@
 import Books from './Books/Application/Queries/Books.js'
 import MergeBook from './Books/Application/Mutations/MergeBook.js'
+import Users from './Users/Application/Queries/Users.js'
+import MergeUser from './Users/Application/Mutations/MergeUser.js'
 
 const resolvers = {
   Query: {
     Books,
+    Users,
   },
   Mutation: {
     MergeBook,
+    MergeUser,
   },
 }
 
--- src/schemas/plathanusSchema/typeDefs.js
+++ src/schemas/plathanusSchema/typeDefs.js
@@ -1,20 +1,27 @@
 import {
   BookDomain, BookQueries, BookMutations, BookInputs,
 } from './Books/Domain/Books.js'
+import {
+  UserDomain, UserQueries, UserMutations, UserInputs,
+} from './Users/Domain/Users.js'
 
 const typeDefs = `
 
 ${BookDomain}
+${UserDomain}
 
 type Query {
   ${BookQueries}
+  ${UserQueries}
 }
 
 type Mutation {
   ${BookMutations}
+  ${UserMutations}
 }
 
 ${BookInputs}
+${UserInputs}
 `
 
 export default typeDefs
